30c2d4d1d5e1fd360c7b46499d7cc7b654abce58
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-26 18:04:58 +0530

    moved hideImage function to index.html from public/js/hideImage file

--- public/js/hideImage.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const hideImage = imageId => {
-  const image = document.getElementById(imageId);
-  image.style['opacity'] = '0';
-  setTimeout(() => (image.style['opacity'] = '1'), 1000);
-};
