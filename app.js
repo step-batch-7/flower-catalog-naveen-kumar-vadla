13392307301fae72af0732b4a0b923d318c9cad6
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-27 11:31:20 +0530

    Added registerCommentAndRedirect
    What changes you have made?
    1. Added registerCommentAndRedirect
    2. Now it will just redirectTo the same page
    
    What is the impact of the change?
    .....

--- app.js
+++ app.js
@@ -6,21 +6,7 @@ const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
 
 const STATIC_FOLDER = `${__dirname}/public`;
-
-const serveStaticFile = (req, optionUrl) => {
-  let path = `${STATIC_FOLDER}${optionUrl || req.url}`;
-  const stat = existsSync(path) && statSync(path);
-  if (!stat || !stat.isFile()) return serveBadRequestPage();
-  const [, extension] = path.match(/.*\.(.*)$/) || [];
-  const contentType = CONTENT_TYPES[extension];
-  const content = readFileSync(path);
-  const res = new Response();
-  res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.body = content;
-  return res;
-};
+const COMMENTS_PATH = `${STATIC_FOLDER}/docs/comments.json`;
 
 const serveBadRequestPage = req => {
   const content = `<html>
@@ -36,8 +22,19 @@ const serveBadRequestPage = req => {
   return res;
 };
 
-const serveHomePage = req => {
-  return serveStaticFile(req, '/index.html');
+const serveStaticFile = req => {
+  let path = `${STATIC_FOLDER}${req.url}`;
+  const stat = existsSync(path) && statSync(path);
+  if (!stat || !stat.isFile()) return serveBadRequestPage();
+  const [, extension] = path.match(/.*\.(.*)$/) || [];
+  const contentType = CONTENT_TYPES[extension];
+  const content = readFileSync(path);
+  const res = new Response();
+  res.setHeader('Content-Type', contentType);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 200;
+  res.body = content;
+  return res;
 };
 
 const serveGuestBookPage = function(req) {
@@ -50,8 +47,25 @@ const serveGuestBookPage = function(req) {
   return res;
 };
 
+const registerCommentAndRedirect = req => {
+  const date = new Date().toGMTString();
+  const { name, comment } = req.body;
+  console.log(date, name, comment);
+  const res = new Response();
+  res.setHeader('Location', '/GuestBook.html');
+  res.setHeader('Content-Length', 0);
+  res.statusCode = 301;
+  return res;
+};
+
+const serveHomePage = req => {
+  req.url = '/index.html';
+  return serveStaticFile(req);
+};
+
 const findHandler = req => {
   if (req.method === 'GET' && req.url === '/') return serveHomePage;
+  if (req.method === 'POST' && req.url === '/registerComment') return registerCommentAndRedirect;
   if (req.method === 'GET' && req.url === '/GuestBook.html') return serveGuestBookPage;
   if (req.method === 'GET') return serveStaticFile;
   return serveBadRequestPage;
