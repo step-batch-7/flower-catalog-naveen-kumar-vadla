d1969c9c17e38786cf16234b1993daedd5100107
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-30 12:20:19 +0530

    Added App classs

--- app.js
+++ app.js
@@ -2,24 +2,44 @@
 
 const HANDLERS = require('./handlers');
 
-const routes = [
-  { path: '/GuestBook.html', handler: HANDLERS.serveGuestBookPage, method: 'GET' },
-  { path: '', handler: HANDLERS.serveStaticFile, method: 'GET' },
-  { path: '', handler: HANDLERS.serveNotFoundPage, method: 'GET' },
-  { path: '/registerComment', handler: HANDLERS.registerCommentAndRedirect, method: 'POST' },
-  { path: '', handler: HANDLERS.serveNotFoundPage, method: 'POST' },
-  { handler: HANDLERS.serveBadRequestPage }
-];
+class App {
+  constructor() {
+    this.routes = [];
+  }
+
+  get(path, handler) {
+    this.routes.push({ path, handler, method: 'GET' });
+  }
+
+  post(path, handler) {
+    this.routes.push({ path, handler, method: 'POST' });
+  }
+
+  use(handler) {
+    this.routes.push({ handler });
+  }
+};
 
 const isRouteMatched = (route, req) => {
-  if (route.method) return route.method === req.method && req.url.match(route.path);
+  if (route.method)
+    return route.method === req.method && req.url.match(route.path);
   return true;
 };
 
 const processRequest = (req, res) => {
   console.warn('Request:', req.url, req.method);
   console.warn(req.headers);
-  const matchedRoutes = routes.filter(route => isRouteMatched(route, req));
+
+  const app = new App();
+  app.get('/GuestBook.html', HANDLERS.serveGuestBookPage);
+  app.get('', HANDLERS.serveStaticFile);
+  app.get('', HANDLERS.serveNotFoundPage);
+  app.post('/registerComment', HANDLERS.registerCommentAndRedirect);
+  app.post('', HANDLERS.serveNotFoundPage);
+  app.use(HANDLERS.serveBadRequestPage);
+
+  const matchedRoutes = app.routes.filter(route => isRouteMatched(route, req));
+
   const next = () => {
     if (matchedRoutes.length === 0) return;
     const route = matchedRoutes.shift();
