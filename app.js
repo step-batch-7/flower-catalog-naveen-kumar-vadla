702138a0a5b456ad1ec578355fa6051415e95bf9
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-30 12:34:34 +0530

    Added handleRequests method in App class and removed processRequests
    What changes you have made?
    1. Added handleRequests method in App class
    2. removed processRequests function
    3. Adding all routes in server
    4. giving handleRequest as listener for Server with binding app

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const HANDLERS = require('./handlers');
-
 class App {
   constructor() {
     this.routes = [];
@@ -18,35 +16,21 @@ class App {
   use(handler) {
     this.routes.push({ handler });
   }
+
+  handleRequests (req, res) {
+    const matchedRoutes = this.routes.filter(route => isRouteMatched(route, req));
+    const next = () => {
+      if (matchedRoutes.length === 0) return;
+      const route = matchedRoutes.shift();
+      route.handler(req, res, next);
+    };
+    next();
+  };
 };
 
 const isRouteMatched = (route, req) => {
-  if (route.method)
-    return route.method === req.method && req.url.match(route.path);
+  if (route.method) return route.method === req.method && req.url.match(route.path);
   return true;
 };
 
-const processRequest = (req, res) => {
-  console.warn('Request:', req.url, req.method);
-  console.warn(req.headers);
-
-  const app = new App();
-  app.get('/GuestBook.html', HANDLERS.serveGuestBookPage);
-  app.get('', HANDLERS.serveStaticFile);
-  app.get('', HANDLERS.serveNotFoundPage);
-  app.post('/registerComment', HANDLERS.registerCommentAndRedirect);
-  app.post('', HANDLERS.serveNotFoundPage);
-  app.use(HANDLERS.serveBadRequestPage);
-
-  const matchedRoutes = app.routes.filter(route => isRouteMatched(route, req));
-
-  const next = () => {
-    if (matchedRoutes.length === 0) return;
-    const route = matchedRoutes.shift();
-    const handler = route.handler;
-    handler(req, res, next);
-  };
-  next();
-};
-
-module.exports = { processRequest };
+module.exports = App;
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const { Server } = require('http');
-const { processRequest } = require('./app');
+const handlers = require('./handlers');
+const App = require('./app');
+
+const app = new App();
+
+app.get('/GuestBook.html', handlers.serveGuestBookPage);
+app.get('', handlers.serveStaticFile);
+app.get('', handlers.serveNotFoundPage);
+
+app.post('/registerComment', handlers.registerCommentAndRedirect);
+app.post('', handlers.serveNotFoundPage);
+
+app.use(handlers.serveBadRequestPage);
 
 const main = (port = 7000) => {
-  const server = new Server(processRequest);
+  const server = new Server(app.handleRequests.bind(app));
   server.listen(port, () => console.log(`started listening: ${port}`));
 };
 
