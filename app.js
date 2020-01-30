6d9a97df09385c6a3d46a2719b364f6335acdeb6
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-01-30 11:40:53 +0530

    moved all handlers to handlers.js
    What changes you have made?
    1. moved all handlers to handlers.js
    2. requiring all hanadlers in app.js
    3. now my app will only find matching handlers and calls them one after another if needed

--- app.js
+++ app.js
@@ -1,104 +1,14 @@
 'use strict';
 
-const { existsSync, readFileSync, statSync, writeFileSync } = require('fs');
-const { loadTemplate } = require('./lib/viewTemplate');
-const queryString = require('querystring');
-const CONTENT_TYPES = require('./lib/mimeTypes');
-
-const STATIC_FOLDER = `${__dirname}/public`;
-const COMMENTS_PATH = `${__dirname}/data/comments.json`;
-
-const serveNotFoundPage = (req, res, next) => {
-  const content = `<html>
-    <head><title>Not Found</title></head>
-    <body>
-      <p>404 File Not Found</p>
-    </body>
-  </html>`;
-  res.setHeader('Content-Type', CONTENT_TYPES.html);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 404;
-  res.end(content);
-};
-
-const serveBadRequestPage = (req, res, next) => {
-  const content = `<html>
-    <head><title>Bad Request</title></head>
-    <body>
-      <p>400 Method not allowed</p>
-    </body>
-  </html>`;
-  res.setHeader('Content-Type', CONTENT_TYPES.html);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 400;
-  res.end(content);
-};
-
-const serveStaticFile = (req, res, next) => {
-  if (req.url === '/') req.url = '/index.html';
-  let path = `${STATIC_FOLDER}${req.url}`;
-  const stat = existsSync(path) && statSync(path);
-  if (!stat || !stat.isFile()) return next();
-  const [, extension] = path.match(/.*\.(.*)$/) || [];
-  const content = readFileSync(path);
-  res.setHeader('Content-Type', CONTENT_TYPES[extension]);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.end(content);
-};
-
-const loadComments = () => {
-  if (existsSync(COMMENTS_PATH)) return JSON.parse(readFileSync(COMMENTS_PATH, 'utf8') || '[]');
-  return [];
-};
-
-const generateCommentsHtml = (commentsHtml, commentDetails) => {
-  const { date, name, comment } = commentDetails;
-  const html = `<tr>
-    <td class = "date">${new Date(date).toGMTString()}</td>
-    <td class = "name">${name}</td>
-    <td class = "comment">${comment.replace(/\r\n/g, '</br>')}</td>
-  </tr>`;
-  return html + commentsHtml;
-};
-
-const serveGuestBookPage = (req, res, next) => {
-  const comments = loadComments();
-  const commentsHtml = comments.reduce(generateCommentsHtml, '');
-  const html = loadTemplate('GuestBook.html', { COMMENTS: commentsHtml });
-  res.setHeader('Content-Type', CONTENT_TYPES.html);
-  res.setHeader('Content-Length', html.length);
-  res.statusCode = 200;
-  res.end(html);
-};
-
-const redirectTo = (url, res) => {
-  res.setHeader('Location', url);
-  res.setHeader('Content-Length', 0);
-  res.statusCode = 301;
-  res.end();
-};
-
-const registerCommentAndRedirect = (req, res, next) => {
-  let data = '';
-  const comments = loadComments();
-  const date = new Date();
-  req.on('data', chunk => (data += chunk));
-  req.on('end', () => {
-    const { name, comment } = queryString.parse(data);
-    comments.push({ date, name, comment });
-    writeFileSync(COMMENTS_PATH, JSON.stringify(comments), 'utf8');
-    redirectTo('./GuestBook.html', res);
-  });
-};
+const HANDLERS = require('./handlers');
 
 const routes = [
-  { path: '/GuestBook.html', handler: serveGuestBookPage, method: 'GET' },
-  { path: '', handler: serveStaticFile, method: 'GET' },
-  { path: '', handler: serveNotFoundPage, method: 'GET' },
-  { path: '/registerComment', handler: registerCommentAndRedirect, method: 'POST' },
-  { path: '', handler: serveNotFoundPage, method: 'POST' },
-  { handler: serveBadRequestPage }
+  { path: '/GuestBook.html', handler: HANDLERS.serveGuestBookPage, method: 'GET' },
+  { path: '', handler: HANDLERS.serveStaticFile, method: 'GET' },
+  { path: '', handler: HANDLERS.serveNotFoundPage, method: 'GET' },
+  { path: '/registerComment', handler: HANDLERS.registerCommentAndRedirect, method: 'POST' },
+  { path: '', handler: HANDLERS.serveNotFoundPage, method: 'POST' },
+  { handler: HANDLERS.serveBadRequestPage }
 ];
 
 const isRouteMatched = (route, req) => {
---
+++ handlers.js
@@ -0,0 +1,99 @@
+'use strict'
+
+const { existsSync, readFileSync, statSync, writeFileSync } = require('fs');
+const { loadTemplate } = require('./lib/viewTemplate');
+const queryString = require('querystring');
+const CONTENT_TYPES = require('./lib/mimeTypes');
+
+const STATIC_FOLDER = `${__dirname}/public`;
+const COMMENTS_PATH = `${__dirname}/data/comments.json`;
+
+
+const serveStaticFile = (req, res, next) => {
+  if (req.url === '/') req.url = '/index.html';
+  let path = `${STATIC_FOLDER}${req.url}`;
+  const stat = existsSync(path) && statSync(path);
+  if (!stat || !stat.isFile()) return next();
+  const [, extension] = path.match(/.*\.(.*)$/) || [];
+  const content = readFileSync(path);
+  res.setHeader('Content-Type', CONTENT_TYPES[extension]);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 200;
+  res.end(content);
+};
+
+const generateCommentsHtml = (commentsHtml, commentDetails) => {
+  const { date, name, comment } = commentDetails;
+  const html = `<tr>
+    <td class = "date">${new Date(date).toGMTString()}</td>
+    <td class = "name">${name}</td>
+    <td class = "comment">${comment.replace(/\r\n/g, '</br>')}</td>
+  </tr>`;
+  return html + commentsHtml;
+};
+
+const serveGuestBookPage = (req, res, next) => {
+  const comments = loadComments();
+  const commentsHtml = comments.reduce(generateCommentsHtml, '');
+  const html = loadTemplate('GuestBook.html', { COMMENTS: commentsHtml });
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', html.length);
+  res.statusCode = 200;
+  res.end(html);
+};
+
+const loadComments = () => {
+  if (existsSync(COMMENTS_PATH))
+    return JSON.parse(readFileSync(COMMENTS_PATH, 'utf8') || '[]');
+  return [];
+};
+
+
+const registerCommentAndRedirect = (req, res, next) => {
+  let data = '';
+  const comments = loadComments();
+  const date = new Date();
+  req.on('data', chunk => (data += chunk));
+  req.on('end', () => {
+    const { name, comment } = queryString.parse(data);
+    comments.push({ date, name, comment });
+    writeFileSync(COMMENTS_PATH, JSON.stringify(comments), 'utf8');
+    redirectTo('./GuestBook.html', res);
+  });
+};
+
+const redirectTo = (url, res) => {
+  res.setHeader('Location', url);
+  res.setHeader('Content-Length', 0);
+  res.statusCode = 301;
+  res.end();
+};
+
+
+const serveNotFoundPage = (req, res, next) => {
+  const content = `<html>
+    <head><title>Not Found</title></head>
+    <body>
+      <p>404 File Not Found</p>
+    </body>
+  </html>`;
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 404;
+  res.end(content);
+};
+
+const serveBadRequestPage = (req, res, next) => {
+  const content = `<html>
+    <head><title>Bad Request</title></head>
+    <body>
+      <p>400 Method not allowed</p>
+    </body>
+  </html>`;
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 400;
+  res.end(content);
+};
+
+module.exports = { serveStaticFile, serveGuestBookPage, registerCommentAndRedirect, serveNotFoundPage, serveBadRequestPage }
