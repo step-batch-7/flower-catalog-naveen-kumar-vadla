ebcd0f3daa239f6c917c18a47c89c30a227ef3c7
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-21 09:36:17 +0530

    Added setUpDataBase

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 'use strict';
 
+const fs = require('fs');
+
 const { Server } = require('http');
 const app = require('./handlers');
 
 const defaultPort = 7000;
 const port = process.env.PORT || defaultPort;
 
-const main = (port) => {
+const setUpDataBase = function() {
+  const DATA_PATH = `${__dirname}/data`;
+  if (!fs.existsSync(DATA_PATH)) {
+    fs.mkdirSync(DATA_PATH);
+  }
+};
+
+const main = port => {
+  setUpDataBase();
   const server = new Server(app.handleRequests.bind(app));
   server.listen(port, () => process.stderr.write(`started listening: ${port}`));
 };
