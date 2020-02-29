f22d2cb53009007ea8b7c40d872f3db45aae6a6f
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-29 10:20:07 +0530

    Moved to express
    What changes you have made?
    1. removed app class and using express default app
    2. moved all routes to app.js
    3. removed handlers and using express default for that
        a. readBody - express.json() and express.urlencoded()
        b. serveStativFile - express.static('public')
        c. serveBadRequest - express default
        d. serveNotFound - express default

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 
 const fs = require('fs');
 
-const { Server } = require('http');
-const app = require('./lib/handlers');
+const app = require('./lib/app');
 
 const defaultPort = 7000;
 const port = process.env.PORT || defaultPort;
@@ -17,8 +16,7 @@ const setUpDataBase = function() {
 
 const main = port => {
   setUpDataBase();
-  const server = new Server(app.handleRequests.bind(app));
-  server.listen(port, () => process.stderr.write(`started listening: ${port}`));
+  app.listen(port, () => process.stderr.write(`started listening: ${port}`));
 };
 
 main(port);
