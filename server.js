412cc777567270a7dc9e424a45386dd0845e44b5
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-02 10:19:17 +0530

    removed setUpDatabase

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-
 const app = require('./lib/app');
 
 const defaultPort = 7000;
 const port = process.env.PORT || defaultPort;
 
-const setUpDataBase = function() {
-  const DATA_PATH = `${__dirname}/data`;
-  if (!fs.existsSync(DATA_PATH)) {
-    fs.mkdirSync(DATA_PATH);
-  }
-};
-
 const main = port => {
-  setUpDataBase();
   app.listen(port, () => process.stderr.write(`started listening: ${port}`));
 };
 
